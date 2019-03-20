2a7199f23409ad2426ebf19ffd9e32b1fbc20edd
PietroBelloPpz <pietro.bello.ppz@gmail.com>
2019-03-20 19:19:44 +0100

    base_url http://www.nannyapp.cloud/

--- platforms/browser/www/assets/custom/js/octocore.js
+++ platforms/browser/www/assets/custom/js/octocore.js
@@ -2,9 +2,9 @@
 
 //var base_url = 'http://localhost.nannyapp.cloud/';
 //var base_url = 'http://localhost.roncatospareparts.octocore.it/';
-//var base_url = 'http://localhostapp.nannyapp.cloud/';
+var base_url = 'http://www.nannyapp.cloud/';
 //var base_url = 'http://192.168.43.111:8061/';
-var base_url = 'http://192.168.1.83:8062/';
+//var base_url = 'http://192.168.1.83:8062/';
 var app_details = null;
 var pages = null;
 var current_user = null;
